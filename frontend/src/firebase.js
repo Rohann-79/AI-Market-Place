aaa23a5c45e8e37d014a298f1f08030e112a8f2d
Rohann-79 <rohannk99999@gmail.com>
2025-03-13 19:06:55 +0530

    API Removed from Expose

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration (replace with your own)
 const firebaseConfig = {
-    apiKey: "AIzaSyDeuftUSYEi3v8FXsi6rFSRc1WetDWCd7I",
-    authDomain: "ai-market-place.firebaseapp.com",
-    projectId: "ai-market-place",
-    storageBucket: "ai-market-place.firebasestorage.app",
-    messagingSenderId: "377788128670",
-    appId: "1:377788128670:web:4cb83b64075619e1064a8f",
-    measurementId: "G-S83QP2RRSW"
+    
 };
 
 // Initialize Firebase
